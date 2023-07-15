11eff3f588ab0d6de6ec3239fcc479f3583120d6
sumairirshad <sumairirshad@gmail.com>
2023-07-15 20:16:59 +0500

    Restaurant Search Screen Skeleton

---
+++ screens/RestaurantSearch.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {StyleSheet, View,Text } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#FFF6F4',
+        justifyContent: 'center'
+    }
+})
+
+const RestaurantSearch = () =>(
+
+   
+
+    // Replace `null` with the `View` component
+     <View style={[styles.container]}>
+    <Text> Hello world</Text>
+    </View>);
+  
+  export default RestaurantSearch;
