3d25cd62d9acbc683df637283908fe5d0fb4570f
sumairirshad <sumairirshad@gmail.com>
2023-07-18 18:18:59 +0500

    Completed Delivered Screen

--- screens/DeliveredScreen.js
+++ screens/DeliveredScreen.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { View, Text, Image, StyleSheet } from 'react-native';
+import AppHeader from '../components/AppHeader'
+import BottomNavbar from '../components/BottomNavbar'
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
   heading: {
     color:'#002B5B',
@@ -13,24 +13,31 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginBottom: 10,
     textAlign:'center'
+   
+  },
+  smallTextView: {
+    alignItems:'center'
   },
   smallText: {
     color:'#292D32',
     fontSize: 14,
-    marginBottom: 20,
+    marginBottom: 20
   }
 
 });
 
-const Delivered = () => {
+const Delivered = ({ navigation }) => {
   return (
     <View style={styles.container}>
+      
+      <AppHeader   navigation={navigation} />
       <Text style={styles.heading}> {'Your Favourite Food,\nDelivered Fast'} </Text>
-      <Text style={styles.smallText}>Thank you for ordering with Us</Text>
+     <View style={styles.smallTextView}><Text style={styles.smallText}>Thank you for ordering with Us</Text></View> 
       <Image
         source={require('../assets/DeliveryRider.png')} // Replace with your image source
         
       />
+       <BottomNavbar />
     </View>
   );
 };
